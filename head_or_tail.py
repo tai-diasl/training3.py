8a89878011ed2628c83fd4b3db0189c9e176242e
TaiDias <103934445+tai-diasl@users.noreply.github.com>
2023-02-08 23:44:33 +0000

    Update head_or_tail.py

--- head_or_tail.py
+++ head_or_tail.py
@@ -1,4 +1,4 @@
-# Create a program that plays Head or Tail
+# Creating a program that plays Head or Tail
 # I could have chosen the numbers 0 and 1, but I wanted to take the opportunity to use the modulus operator.
 
 import random
