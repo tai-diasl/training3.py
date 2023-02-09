ada8778e9ccaf7776022ba2738003f6e1266003a
Taís Dias <taisayana@hotmail.com>
2023-02-09 21:02:47 +0000

    Odd or Even Program

---
+++ bmi_calculator.py
@@ -0,0 +1,19 @@
+print("| :: | BMI Calculator | :: |\n")
+
+height = float(input("Please, enter your height in m: "))
+weight = float(input("Now, enter your weight in kg: "))
+
+bmi = round(weight / (height ** 2), 1)
+
+if bmi < 18.5:
+    status = "you are underweight"
+elif bmi < 25:
+    status = "you have a normal weight"
+elif bmi < 30:
+    status = "you are slightly overweight"
+elif bmi < 35:
+    status = "you are obese"
+else:
+    status = "you are clinically obese"
+
+print(f"\nYour BMI is {bmi}, {status}.")
